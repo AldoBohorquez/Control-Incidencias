06f5ee63c17c357166ec5212e9575fafcec225b2
Fabian <cruzfabianhiram@gmail.com>
2024-05-23 15:40:54 -0600

    Creation of the page userAssigned

---
+++ frontend/src/app/pages/user-assigned/user-assigned.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-assigned',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-assigned.component.html',
+  styleUrl: './user-assigned.component.css'
+})
+export class UserAssignedComponent {
+
+}
