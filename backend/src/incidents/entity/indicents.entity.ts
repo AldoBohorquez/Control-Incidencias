9aad659d1e10bf7ead8daf4bbfcd8a5350fb409f
Aldo Bohorquez <160769617+AldoBohorquez@users.noreply.github.com>
2024-05-23 13:22:44 -0600

    entity incident

--- backend/src/incidents/entity/indicents.entity.ts
+++ backend/src/incidents/entity/indicents.entity.ts
@@ -1,11 +1,23 @@
+import { DateshEntity } from "src/dates-h/entity/dates-h.entity";
 import { UsersEntity } from "src/users/entity/users.entity";
-import { Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('incidents')
 export class IncidentsEntity
 {
     @PrimaryGeneratedColumn()
     id_incident: number;
+
+    @Column({type: 'varchar', length: 60})
+    titulo:string;
+
+    @Column({type:'varchar',length:200})
+    descripcion:string;
+
+    @Column()
+
+    @OneToMany(()=>DateshEntity,(dateh)=>dateh.incident)
+    dateh:DateshEntity[];
 
     @ManyToOne(()=>UsersEntity,(user)=>user.incidents)
     user:UsersEntity;
