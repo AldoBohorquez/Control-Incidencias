cd7dab038300319a490811d233f17d227517afe0
Aldo Bohorquez <160769617+AldoBohorquez@users.noreply.github.com>
2024-05-23 14:28:09 -0600

    avanze entity solutions

--- backend/src/solutions/solutions.service.ts
+++ backend/src/solutions/solutions.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { SolutionsEntity } from './entity/solutions.entity';
 
 @Injectable()
-export class SolutionsService {}
+export class SolutionsService {
+
+    constructor(private dataSource:DataSource) {}
+
+    async getSolutions()
+    {
+        try {
+            const solutionsFind = await this.dataSource.getRepository(SolutionsEntity).find({relations:['incidents']})
+        } catch (error) {
+            throw new HttpException('Error al consultar los datos',HttpStatus.BAD_REQUEST,error)
+        }
+    }
+}
