c750328beae45a443d8c435a05961fdd6c78ec2c
Aldo Bohorquez <160769617+AldoBohorquez@users.noreply.github.com>
2024-05-23 11:33:36 -0600

    Configuracion de librerias y base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AdminModule } from './admin/admin.module';
+import { UsersModule } from './users/users.module';
+import { IncidentsModule } from './incidents/incidents.module';
+import { StatusModule } from './status/status.module';
+import { SolutionsModule } from './solutions/solutions.module';
+import { TestsModule } from './tests/tests.module';
+import { DatesHModule } from './dates-h/dates-h.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "monorail.proxy.rlwy.net",
+    port: 5432,
+    username: "postgres",
+    password: "DJOQBQPmzsJDIogcIImOrAfEEywGvUxV",
+    database: "railway",
+    synchronize: true,
+    logging: true,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+  }), AdminModule, UsersModule, IncidentsModule, StatusModule, SolutionsModule, TestsModule, DatesHModule
+    ,],
+  
   controllers: [AppController],
   providers: [AppService],
 })
