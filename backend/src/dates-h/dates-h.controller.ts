336d2478515bdf60087da2b2a3e3c06600da15c6
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-05-23 14:42:27 -0600

    datesh completo ahorasi real xd

--- backend/src/dates-h/dates-h.controller.ts
+++ backend/src/dates-h/dates-h.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { DatesHService } from './dates-h.service';
+import { DateshDto } from './dto/dates-h.dto';
 
 @Controller('dates-h')
-export class DatesHController {}
+export class DatesHController {
+    constructor(private serviceDatesh:DatesHService){}
+
+    @Get()
+    getDatesH(){
+        return this.serviceDatesh.getDatesH();
+    }
+    @Get('id')
+    getDatesh(@Param('id_Dates_h') id:number){
+        return this.serviceDatesh.getDatesh(id);
+    }
+    @Post()
+    createDatesh(@Body() datesh:DateshDto){
+        return this.serviceDatesh.createDatesh(datesh);
+    }
+    @Put('id')
+    updateDatesh(@Param('id') id:number,@Body() datesh:DateshDto){
+        return this.serviceDatesh.updateDatesh(id,datesh);
+    }
+    @Delete('id')
+    deleteDatesh(@Param('id') id:number){
+        return this.serviceDatesh.deleteDatesh(id);
+    }
+}
