ec6f1d0d0cb5a9b7871945e16df92be22bc534e4
nayeliyici <125396720+nayeliyici@users.noreply.github.com>
2024-05-23 20:18:07 -0600

    finished user Finished

---
+++ frontend/src/app/pages/user-finished/user-finished.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-finished',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-finished.component.html',
+  styleUrl: './user-finished.component.css'
+})
+export class UserFinishedComponent {
+
+}
