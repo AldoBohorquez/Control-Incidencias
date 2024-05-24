6e650cf11c5a66eaab9fc815abec237d3d9242a4
Aldo Bohorquez <160769617+AldoBohorquez@users.noreply.github.com>
2024-05-23 22:45:12 -0600

    cambios correctos

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -7,130 +7,69 @@ import { AdminEntity } from 'src/admin/entity/admin.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(private dataSource: DataSource) {}
 
-  async getUsers() {
-    try {
-      const usersFind = await this.dataSource
-        .getRepository(UsersEntity)
-        .find({
-          relations: ['incidents'],
-          select: [
-            'id_user',
-            'nombre',
-            'apPat',
-            'apMat',
-            'correo',
-            'area',
-            'puesto',
-          ],
-        });
+    constructor(private dataSource:DataSource) { }
 
-      if (!usersFind) {
-        return new HttpException(
-          'No se encontraron usuarios',
-          HttpStatus.NOT_FOUND,
-        );
-      }
+    async getUsers()
+    {
+        try {
+            const usersFind = await this.dataSource.getRepository(UsersEntity).find({relations:['incidents'],select:['id_user','nombre','apPat','apMat','correo','area','puesto']});
 
-      return usersFind;
-    } catch (error) {
-      throw new HttpException(
-        'Error al consultar los datos',
-        HttpStatus.BAD_REQUEST,
-        error,
-      );
+            if(!usersFind)
+            {
+                return new HttpException('No se encontraron usuarios',HttpStatus.NOT_FOUND)
+            }
+
+            return usersFind;
+        } catch (error) {
+            throw new HttpException('Error al consultar los datos',HttpStatus.BAD_REQUEST,error)
+        }
     }
-  }
 
-  async getUser(id_user: number) {
-    try {
-      const userFind = await this.dataSource
-        .getRepository(UsersEntity)
-        .findOne({
-          where: { id_user: id_user },
-          relations: ['incidents'],
-          select: [
-            'id_user',
-            'nombre',
-            'apPat',
-            'apMat',
-            'correo',
-            'area',
-            'puesto',
-          ],
-        });
+    async getUser(id_user:number)
+    {
+        try {
+            const userFind = await this.dataSource.getRepository(UsersEntity).findOne({where:{id_user:id_user},relations:['incidents'],select:['id_user','nombre','apPat','apMat','correo','area','puesto']});
 
-      if (!userFind) {
-        return new HttpException(
-          'No se encontro el usuario',
-          HttpStatus.NOT_FOUND,
-        );
-      }
+            if(!userFind)
+            {
+                return new HttpException('No se encontro el usuario',HttpStatus.NOT_FOUND)
+            }
 
-      return userFind;
-    } catch (error) {
-      throw new HttpException(
-        'Error al consultar los datos',
-        HttpStatus.BAD_REQUEST,
-        error,
-      );
+            return userFind;
+        } catch (error) {
+            throw new HttpException('Error al consultar los datos',HttpStatus.BAD_REQUEST,error)
+        }
     }
-  }
 
-  async createUser(user: UsersDto) {
-    try {
-      const bodyUser = this.dataSource.getRepository(UsersEntity).create(user);
+    async createUser(user:UsersDto)
+    {
+        try {
+            const bodyUser = this.dataSource.getRepository(UsersEntity).create(user);
+            
 
-      return await this.dataSource.getRepository(UsersEntity).save(bodyUser);
-    } catch (error) {
-      console.log(error);
-
-      throw new HttpException(
-        'Error al guardar los datos',
-        HttpStatus.BAD_REQUEST,
-        error,
-      );
+            return await this.dataSource.getRepository(UsersEntity).save(bodyUser);
+        } catch (error) {
+            throw new HttpException('Error al guardar los datos',HttpStatus.BAD_REQUEST,error)
+        }
     }
-  }
 
-  async updateUser(id_user: number, user: UsersDto) {
-    try {
-      const userFind = await this.dataSource
-        .getRepository(UsersEntity)
-        .findOne({ where: { id_user: id_user } });
+    async updateUser(id_user:number,user:UsersDto)
+    {
+        try {
+            const userFind = await this.dataSource.getRepository(UsersEntity).findOne({where:{id_user:id_user}});
 
-      if (!userFind) {
-        return new HttpException(
-          'No se encontro el usuario',
-          HttpStatus.NOT_FOUND,
-        );
-      }
+            if(!userFind)
+            {
+                return new HttpException('No se encontro el usuario',HttpStatus.NOT_FOUND)
+            }
 
-      return await this.dataSource
-        .getRepository(UsersEntity)
-        .update({ id_user: userFind.id_user }, user);
-    } catch (error) {
-      throw new HttpException(
-        'Error al actualizar los datos',
-        HttpStatus.BAD_REQUEST,
-        error,
-      );
+            return await this.dataSource.getRepository(UsersEntity).update({id_user:userFind.id_user},user);
+        } catch (error) {
+            throw new HttpException('Error al actualizar los datos',HttpStatus.BAD_REQUEST,error)
+        }
     }
-  }
 
-  async deleteUser(id_user: number) {
-    try {
-      const userFind = await this.dataSource
-        .getRepository(UsersEntity)
-        .findOne({ where: { id_user: id_user } });
-
-      if (!userFind) {
-        return new HttpException(
-          'No se encontro el usuario',
-          HttpStatus.NOT_FOUND,
-        );
-      }
     async deleteUser(id_user:number)
     {
         try {
@@ -154,52 +93,29 @@ export class UsersService {
                 await this.dataSource.getRepository(IncidentsEntity).save(element);
             });
 
-      return await this.dataSource.getRepository(UsersEntity).remove(userFind);
-    } catch (error) {
-      throw new HttpException(
-        'Error al eliminar los datos',
-        HttpStatus.BAD_REQUEST,
-        error,
-      );
+            return await this.dataSource.getRepository(UsersEntity).remove(userFind);
+        } catch (error) {
+            throw new HttpException('Error al eliminar los datos',HttpStatus.BAD_REQUEST,error)
+        }
     }
-  }
 
-  async encryptPassword(password: string) {
-    const bcrypt = require('bcrypt');
-    const saltRounds = 10;
-    const salt = bcrypt.genSaltSync(saltRounds);
-    return bcrypt.hashSync(password, salt);
-  }
+    async encryptPassword(password:string)
+    {
+        const bcrypt = require('bcrypt');
+        const saltRounds = 10;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        return bcrypt.hashSync(password,salt);
+    }
 
-  async login(correo: string, password: string) {
-    try {
-      const userFind = await this.dataSource
-        .getRepository(UsersEntity)
-        .findOne({ where: { correo: correo } });
     async login(correo:string,password:string)
     {
         let aux:boolean = false;
         try {
             const userFind = await this.dataSource.getRepository(UsersEntity).findOne({where:{correo:correo}});
 
-      const adminFind = await this.dataSource
-        .getRepository(AdminEntity)
-        .findOne({ where: { correo: correo } });
+            const adminFind = await this.dataSource.getRepository(AdminEntity).findOne({where:{correo:correo}});
 
             if(userFind || adminFind)
-      if (!userFind || !adminFind) {
-        return new HttpException(
-          'Correo no registrado',
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-            if(!userFind)
-            {
-                aux = false;
-            }
-
-            if(!adminFind)
             {
                 aux = true;
             }
@@ -209,58 +125,36 @@ export class UsersService {
                 return new HttpException('Correo no registrado',HttpStatus.BAD_REQUEST)
             }
 
-      if (userFind) {
-        const bcrypt = require('bcrypt');
-        const compare = bcrypt.compare(password, userFind.password);
+            if(userFind)
+            {
+                const bcrypt = require('bcrypt');
+                const compare = bcrypt.compare(password,userFind.password);
 
-        if (!compare) {
-          return new HttpException(
-            'Contraseña incorrecta',
-            HttpStatus.BAD_REQUEST,
-          );
+                if(!compare)
+                {
+                    return new HttpException('Contraseña incorrecta',HttpStatus.BAD_REQUEST)
+                }
+
+                return {id:userFind.id_user,nombre:userFind.nombre,apPat:userFind.apPat,apMat:userFind.apMat,correo:userFind.correo,area:userFind.area,puesto:userFind.puesto}
+            }
+
+            if(adminFind)
+            {
+                const bcrypt = require('bcrypt');
+                const compare = bcrypt.compare(password,adminFind.password);
+
+                if(!compare)
+                {
+                    return new HttpException('Contraseña incorrecta',HttpStatus.BAD_REQUEST)
+                }
+
+                return {id:adminFind.id_admin,nombre:adminFind.nombre,apPat:adminFind.apPat,apMat:adminFind.apMat,correo:adminFind.correo, adminB:adminFind.adminB}
+            }
+
+            return new HttpException('Error al iniciar sesion',HttpStatus.BAD_REQUEST)
+        } catch (error) {
+            throw new HttpException('Error al consultar los datos',HttpStatus.BAD_REQUEST,error)
         }
-
-        return {
-          id: userFind.id_user,
-          nombre: userFind.nombre,
-          apPat: userFind.apPat,
-          apMat: userFind.apMat,
-          correo: userFind.correo,
-          area: userFind.area,
-          puesto: userFind.puesto,
-        };
-      }
-
-      if (adminFind) {
-        const bcrypt = require('bcrypt');
-        const compare = bcrypt.compare(password, adminFind.password);
-
-        if (!compare) {
-          return new HttpException(
-            'Contraseña incorrecta',
-            HttpStatus.BAD_REQUEST,
-          );
-        }
-
-        return {
-          id: adminFind.id_admin,
-          nombre: adminFind.nombre,
-          apPat: adminFind.apPat,
-          apMat: adminFind.apMat,
-          correo: adminFind.correo,
-        };
-      }
-
-      return new HttpException(
-        'Error al iniciar sesion',
-        HttpStatus.BAD_REQUEST,
-      );
-    } catch (error) {
-      throw new HttpException(
-        'Error al consultar los datos',
-        HttpStatus.BAD_REQUEST,
-        error,
-      );
     }
-  }
+
 }
