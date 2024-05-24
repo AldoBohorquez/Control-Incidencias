1db3501eea137a4ca1894d054b29f73fbc10bb5b
Aldo Bohorquez <160769617+AldoBohorquez@users.noreply.github.com>
2024-05-24 04:30:26 -0600

    user-panel
    query params para pasar los paramtros

--- frontend/src/app/pages/admin-list/admin-list.component.ts
+++ frontend/src/app/pages/admin-list/admin-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarUserComponent } from '../../shared/navbar-user/navbar-user.component';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-admin-list',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './admin-list.component.html',
   styleUrl: './admin-list.component.css'
 })
