f9b8e03c518f266b004534489585fdfab874172c
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-05-24 00:08:52 -0600

    cambios

---
+++ frontend/src/app/pages/admin-create-user/admin-create-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-create-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-create-user.component.html',
+  styleUrl: './admin-create-user.component.css'
+})
+export class AdminCreateUserComponent {
+
+}
