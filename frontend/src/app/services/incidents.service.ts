c9d7211b60158c3395317ed3b18d49aee4337af8
Aldo Bohorquez <160769617+AldoBohorquez@users.noreply.github.com>
2024-05-23 23:24:44 -0600

    activar corse

--- frontend/src/app/services/incidents.service.ts
+++ frontend/src/app/services/incidents.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, Signal, computed, effect, inject, signal } from '@angular/core';
+import { Incidents } from '../interfaces/incidents.interface';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,41 @@ export class IncidentsService {
 
   private http = inject(HttpClient);
 
-  constructor() { }
+  private incidentSignal = signal<Array<Incidents>>([]);
+
+  constructor() {
+    effect(()=>
+    {
+      this.getIncidents();
+    })
+  }
+
+  get incidents():Signal <Array<Incidents> | []>
+  {
+    return computed(()=>this.incidentSignal())
+  }
+
+  set incidents(value:Array<Incidents>)
+  {
+    this.incidentSignal.update((s:Array<Incidents> | [])=>value)
+  }
+
+  async getIncidents()
+  {
+
+    this.http.get<Array<Incidents>>('http://localhost:3000/incidents').pipe(
+      catchError((error)=>{
+        console.log(error);
+        return [];
+      }
+    )).subscribe(
+    {
+      next:(response)=>
+        {
+          this.incidents = response;
+        }
+    })
+  }
+
+
 }
